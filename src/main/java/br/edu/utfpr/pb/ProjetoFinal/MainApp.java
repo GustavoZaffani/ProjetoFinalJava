fabe357b053d4f0bf2d7accd449f13f0a7991100
g_zaffani@hotmail.com <gu18071999>
2019-06-21 09:28:46 -0300

    Initial commit

---
+++ src/main/java/br/edu/utfpr/pb/ProjetoFinal/MainApp.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.ProjetoFinal;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Entrou aqui");
+        System.exit(0);
+    }
+}
+
